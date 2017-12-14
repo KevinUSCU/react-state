13381b71c0e8ed23e3c9ca2bfbe31c57b1990102
Kevin Springer <KevinUSCU@outlook.com>
2017-12-14 14:39:39 -0800

    Update with breakout progress.

--- src/components/Greetings.js
+++ src/components/Greetings.js
@@ -1,18 +1,40 @@
 import React, { Component } from 'react';
 
 
-const greeting = 'Greetings';
-
 class Greetings extends Component{
   constructor(props){
     super(props)
+
+    this.state = { greeting: ['Roger']}
+
+    // this.handleClick = this.handleClick.bind(this) //one way to attach the this from the button click to this object
+  }
+
+  // handleClick(e) {
+  //   this.setState({ greeting: this.state.greeting + '!' })
+  // }
+
+  // handleClick = (e) => { //another way to attach the this
+  //   this.setState({ greeting: this.state.greeting + '!' })
+  // }
+
+  handleSubmit = (e) => { //another way to attach the this
+    e.preventDefault()
+    this.setState({ greeting: [e.target.personName.value, ...this.state.greeting] })
+    e.target.personName.value = '' //clears out the input box after submit
   }
 
   render(){
     return (
       <div>
+        <form onSubmit={this.handleSubmit}>
+          <input name='personName' />
+          <button>Click me!</button>
+          {/* <button onClick={this.handleClick}>Click me!</button> */}
+          {/* <button onClick={(e) => {this.handleClick(e)}}>Click me!</button> //another way to attach this */}
+        </form>
         <div>
-          { greeting }
+          { this.state.greeting }
         </div>
       </div>
     )
